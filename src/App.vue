9bd8f39ed47687965fbeadb5d9a4864ce9e18cdf
Ulisses Mesquita <desenvolvimento@xpon.com.br>
2024-09-18 07:32:07 -0300

    merge

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="columns is-gapless is-multiline">
     <div class="column is-one-quarter">
-        ????
+        <barraLateral />
     </div>
     <div class="column is-three-quarter">
         ??????
@@ -12,9 +12,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import barraLateral from './/components/barraLateral.vue'
+
 
 export default defineComponent({
   name: 'App',
+  components: {
+    barraLateral  
+  }
 });
 </script>
 
